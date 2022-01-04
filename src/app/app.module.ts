29163eabb2b10af0ee016554f4c837a9bf48e779
Gautam Chopda <chopdagautam92@gmail.com>
2022-01-04 16:44:49 +0530

    contact list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './core/material-module';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  entryComponents: [
+    AddContactComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
